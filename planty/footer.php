8c69cd2340a5817cfdeabf7af8f45e7b7fbddb34
abdelmaleksamat <abdelmaleksamat14@gmail.com>
2023-07-16 13:35:32 +0200

    init

---
+++ planty/footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+  <a id="mentions-legales-link" href="#">Mentions légales</a>
+  <div id="mentions-legales-content">
+    <h2>Mentions légales</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo facilisis sagittis.</p>
+    <!-- Autres éléments de la page des mentions légales -->
+  </div>
+</footer>
