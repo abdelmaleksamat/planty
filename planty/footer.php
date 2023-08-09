0ada59150c35ab76da6de3c68252246617725409
abdelmaleksamat <125268777+abdelmaleksamat@users.noreply.github.com>
2023-08-09 20:54:38 +0200

    Update and rename footer-planty.php to footer.php

---
+++ planty/footer.php
@@ -0,0 +1,10 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+<p><a href="#">Mentions légales</a></p>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
